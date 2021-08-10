ea8ff48b9063611d96426782770956c0263777dd
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2021-08-10 14:51:21 +0600

    Modal create to transfer data

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -10,13 +13,39 @@ import { NavController } from '@ionic/angular';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  place: Place;
+
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController,
+    private placesService: PlacesService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    // this.route.paramMap.subscribe(paramMap => {
+    //   if (!paramMap.has('placeId')) {
+    //     this.navCtrl.navigateBack('/places/tabs/discover');
+    //     return;
+    //   }
+    //   this.place = this.placesService.getPlace(paramMap.get('placeId'));
   }
   onBookPlace(){
     // this.router.navigateByUrl('/places/tabs/discover');
     // this.navCtrl.pop();
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.navigateBack('/places/tabs/discover');
+    this.modalCtrl
+    .create({
+      component: CreateBookingComponent,
+      componentProps: {selectedPlace: this.place}
+    })
+    .then(modalEl => {
+      modalEl.present();
+    });
   }
 }
+  // function onBookPlace(): (error: any) => void {
+  //   throw new Error('Function not implemented.');
+  // }
+
